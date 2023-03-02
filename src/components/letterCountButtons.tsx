a55ce40d8fd891fb2fefe87e869996d5dc4d1587
Kevin Lowe <lowekevin123@gmail.com>
2023-03-01 22:07:31 -0800

    Add generate wordle button

--- src/components/letterCountButtons.tsx
+++ src/components/letterCountButtons.tsx
@@ -7,29 +7,36 @@ export const LetterCountButtons: React.FC = () => {
   const letterCounts = [3, 4, 5, 6, 7, 8];
   const [selectedCount, setSelectedCount] = React.useState(5);
 
+  const handleGenerateClick = React.useCallback(
+    () => console.log("generating wordle", selectedCount),
+    [selectedCount]
+  );
   return (
-    <span className="isolate inline-flex rounded-md shadow-sm">
-      {letterCounts.map((letters) => {
-        const buttonClassNames = clsx(
-          "relative first:rounded-l-md last:rounded-r-md inline-flex items-center border px-4 py-2 text-xl font-medium hover:dark:bg-gray-900",
-          {
-            "z-10 border-indigo-500 outline-none ring-1 ring-indigo-500":
-              letters === selectedCount,
-          },
-          {
-            "border-gray-300": letters !== selectedCount,
-          }
-        );
-        return (
-          <LetterCountButton
-            key={letters}
-            className={buttonClassNames}
-            onLetterClick={setSelectedCount}
-            letters={letters}
-          />
-        );
-      })}
-    </span>
+    <div className="flex flex-col">
+      <span className="isolate mb-4 inline-flex rounded-md shadow-sm">
+        {letterCounts.map((letters) => {
+          const buttonClassNames = clsx(
+            "relative first:rounded-l-md last:rounded-r-md inline-flex items-center border px-4 py-2 text-xl font-medium hover:dark:bg-gray-900",
+            {
+              "z-10 border-indigo-500 outline-none ring-1 ring-indigo-500":
+                letters === selectedCount,
+            },
+            {
+              "border-gray-300": letters !== selectedCount,
+            }
+          );
+          return (
+            <LetterCountButton
+              key={letters}
+              className={buttonClassNames}
+              onLetterClick={setSelectedCount}
+              letters={letters}
+            />
+          );
+        })}
+      </span>
+      <GenerateWordleButton onClick={handleGenerateClick} />
+    </div>
   );
 };
 
@@ -50,6 +57,20 @@ const LetterCountButton: React.FC<{
       onClick={handleClick}
     >
       {letters}
+    </button>
+  );
+};
+
+const GenerateWordleButton: React.FC<{
+  onClick: () => void;
+}> = ({ onClick }) => {
+  return (
+    <button
+      type="button"
+      className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+      onClick={onClick}
+    >
+      Generate Wordle
     </button>
   );
 };
