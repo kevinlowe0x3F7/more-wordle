e70274408689c549ba8df82ad529d262921aecbf
Kevin Lowe <lowekevin123@gmail.com>
2023-03-04 13:09:07 -0800

    Guess box letter styling

--- src/components/guessBoxes.tsx
+++ src/components/guessBoxes.tsx
@@ -12,7 +12,7 @@ export const GuessBoxes: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const boxes = [...Array(numGuesses)].flatMap((_, g) =>
     [...Array(letters)].map((_, i) => (
-      <GuessBox key={`${g}:${i}`} letter={undefined} />
+      <GuessBox key={`${g}:${i}`} letter={g % 2 === 0 ? "A" : undefined} />
     ))
   );
   const classNames = clsx("grid gap-1", getGridColClassName(letters));
@@ -21,7 +21,8 @@ export const GuessBoxes: React.FC = () => {
 
 const GuessBox: React.FC<{ letter: string | undefined }> = ({ letter }) => {
   const classNames = clsx(
-    "flex items-center justify-center h-10 w-10 sm:w-16 sm:h-16 border-2 border-white"
+    "flex items-center justify-center h-10 w-10 sm:w-16 sm:h-16 border-2 text-3xl font-bold",
+    { "border-gray-600": letter == null, "border-gray-200": letter != null }
   );
   return <div className={classNames}>{letter}</div>;
 };
