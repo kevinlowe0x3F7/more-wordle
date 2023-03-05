afdf5dfb330e32523abf62eb3ad4ff053a518227
Kevin Lowe <lowekevin123@gmail.com>
2023-03-05 09:00:50 -0800

    Add coloring implementation

---
+++ src/utils/guesses.ts
@@ -0,0 +1,27 @@
+export type GuessColoring = "not-present" | "correct" | "wrong-location";
+
+export function calculateGuessColoring(guess: string, answer: string) {
+  if (guess.length !== answer.length) {
+    return [...guess].map(_char => "not-present");
+  }
+
+  const coloringsMap = new Map<number, GuessColoring>();
+  for (let i = 0; i < guess.length; i++) {
+    if (guess.charAt(i) === answer.charAt(i)) {
+      coloringsMap.set(i, "correct");
+    }
+  }
+
+  for (let i = 0; i < guess.length; i++) {
+    if (answer.indexOf(guess.charAt(i)) !== -1 && !coloringsMap.has(i)) {
+      coloringsMap.set(i, "wrong-location");
+    } 
+  }
+
+  const colorings: GuessColoring[] = [];
+  for (let i = 0; i < guess.length; i++) {
+    colorings.push(coloringsMap.get(i) ?? "not-present");
+  }
+
+  return colorings;
+}
